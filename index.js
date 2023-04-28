a822e76cb8dc1f580af36a3b123e9a3b5056ee26
kin ho <hokin990@gmail.com>
2023-04-27 20:42:56 -0700

    added js code

---
+++ index.js
@@ -0,0 +1,75 @@
+const inquirer = require('inquirer');
+
+const fs =  require('fs');
+
+const { Triangle, Square, Circle } = require('./lib/shapes.js');
+const { choices } = require('yargs');
+
+function writeToFile (answers) {
+let sgvString = '';
+sgvString += `<svg width="350" height="60" xmlns="http://www.w3.org/2000/svg">`
+
+sgvString += `<g>`
+
+// sgvString += `${answers.shape}`
+let pickedShape;
+if (answers.shape === 'Triangle') {
+    // pickedShape = new Triangle()
+    sgvString 
+    += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+}
+if (answers.shape === 'Square') {
+    pickedShape = new Square()
+    sgvString += `<rect x="73" y="40" width="160" height="160"${answers.shapeBackgroundColor}"/>`;
+}
+if (answers.shape === 'Circle') {
+    pickedShape = new Circle()
+    sgvString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+}
+sgvString += `<text x="10" y="10" fill="${answers.textColor}">${answers.text}</text>`
+
+sgvString += `</g>`
+
+sgvString += `</sgv>`
+
+fs.writeFile('logo.sgv', sgvString, (err) => {
+    err ? console.log(err) : console.log('Sucessfully generated log.sgv file');
+})
+
+}
+ 
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'what text do you want to use? (enter up to three characters)'
+
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'what text color do you want to use? (enter color keyword or a hexadecimal number)'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'what shape do you want to pick?',
+        choices: ['Circle', 'Triangle', 'Square']
+    },
+    {
+        type: 'input',
+        name: 'shapeBackgroundColor',
+        message: 'Pick shape color (Enter keyword OR hexadecimal number)',
+      
+    },
+]
+inquirer 
+.prompt(questions)
+.then((answers) => {
+   if  (answers.text.length > 3){
+    console.log('please enter no more than three characters')
+   } else {
+writeToFile(answers);
+   }
+   
+});
